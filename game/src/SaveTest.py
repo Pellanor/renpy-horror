8a72ee9993b7b462470f6b647a2849cc140a5b8d
Pellanor <pellanor@gmail.com>
2018-08-19 03:21:44 -0600

    Actually include the new python classes used for testing

---
+++ game/src/SaveTest.py
@@ -0,0 +1,21 @@
+from SaveTestNested import SaveTestNested
+
+
+class SaveTest:
+    def __init__(self, flag=False):
+        self.nest = None
+        self.flag = flag
+        self.list = []
+
+    def __str__(self):
+        if self.nest:
+            return "Nested " + str(self.nest)
+        if self.flag:
+            return "Modified save test\n" + '(%s)' % ', '.join(map(str, self.list))
+        return "SaveTest String\n" + '(%s)' % ', '.join(map(str, self.list))
+
+    def add(self, s):
+        self.list.append(s)
+
+    def nested(self, s):
+        self.nest = SaveTestNested(s)
---
+++ game/src/SaveTestNested.py
@@ -0,0 +1,6 @@
+class SaveTestNested:
+    def __init__(self, s):
+        self.s = s
+
+    def __str__(self):
+        return "Nested " + self.s
